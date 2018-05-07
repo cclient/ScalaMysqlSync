83af7487e356abd552f49cdb18d10f6078605031
cclient <cclient@cclients-MacBook-Pro.local>
2018-05-07 20:12:39 +0800

    mysql utf8mb4

--- src/main/scala/main/Conf.scala
+++ src/main/scala/main/Conf.scala
@@ -9,11 +9,10 @@ import slick.jdbc.MySQLProfile.api._
   */
 
 object Conf {
-//  val parsedConfig = ConfigFactory.parseFile(new File("src/main/application.conf"))
-//  val conf = ConfigFactory.load(parsedConfig)
-//  val ORORIGIN_DBIGIN_DB = Database.forConfig("mysqlorginal", config = conf)
-  val ORIGIN_DB = Database.forURL("jdbc:mysql://localhost:3307/csdn?characterEncoding=UTF-8&charset=utf8mb4", user = "root", password = "passwd", driver = "com.mysql.jdbc.Driver",executor = AsyncExecutor("orgindb", numThreads=2, queueSize=1000))
-  val DEST_DB = Database.forURL("jdbc:mysql://localhost:3306/geekbang?characterEncoding=UTF-8&charset=utf8mb4", user = "root", password = "passwd", driver = "com.mysql.jdbc.Driver")
+//  val ORIGIN_DB = Database.forURL("jdbc:mysql://localhost:3307/csdn?characterEncoding=UTF-8&charset=utf8mb4", user = "root", password = "passwd", driver = "com.mysql.jdbc.Driver",executor = AsyncExecutor("orgindb", numThreads=2, queueSize=1000))
+  val ORIGIN_DB = Database.forConfig("mysql_orginal")
+//  val DEST_DB = Database.forURL("jdbc:mysql://localhost:3306/geekbang?characterEncoding=UTF-8&charset=utf8mb4", user = "root", password = "passwd", driver = "com.mysql.jdbc.Driver")
+  val DEST_DB = Database.forConfig("mysql_dest")
   val PAGE_SIZE = 200
   val DEFAULT_DATE = new Date(2016, 1, 1)
 }
